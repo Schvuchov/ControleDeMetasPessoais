18d29b614d5c911d1b0ed69c1987ee082bdf2444
Barbara Schvuchov <schvuchov.k@gmail.com>
2024-09-12 13:00:33 -0300

    funcionalidade e rota create-goal

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,6 +1,25 @@
 import fastify from "fastify";
+import { createGoal } from "../functions/create-goal";
+import z from 'zod'
 
 const app = fastify()
+
+//criando rota para add goals
+app.post('/goals', async (request) => {
+
+    //zod para criar a validação 
+    const createGoalSchema = z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+    })
+
+    const body = createGoalSchema.parse(request.body) //parse : caso não tenha as inf ele vai dar erro
+
+    await createGoal({
+        title: body.title,
+        desiredWeeklyFrequency: body.desiredWeeklyFrequency,
+    })
+})
 
 app.listen({
     port: 3333,
