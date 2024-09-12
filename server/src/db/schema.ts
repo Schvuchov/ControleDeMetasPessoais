a836e5e277c60a1bd59cb179586b9a461ea9e2e5
Barbara Schvuchov <schvuchov.k@gmail.com>
2024-09-11 21:55:13 -0300

    config básicas para começar a aplicação

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,7 +1,8 @@
 import { pgTable, text, integer, timestamp } from 'drizzle-orm/pg-core'
+import { createId } from "@paralleldrive/cuid2"
 
 export const goals = pgTable('goals', {
-  id: text('id').primaryKey(),
+  id: text('id').primaryKey().$defaultFn(() => createId()),   //.$defaultFn(() => createId()) pra usar o criador de id unico
   title: text('title').notNull(),
   desiredWeeklyFrequency: integer('desired_weekly_frequency').notNull(),
   createdAt: timestamp('created_at', { withTimezone: true })
@@ -10,7 +11,7 @@ export const goals = pgTable('goals', {
 })
 
 export const goalCompletions = pgTable('goal_completions', {
-  id: text('id').primaryKey(),
+  id: text('id').primaryKey().$defaultFn(() => createId()),
   goalId: text('goal_id')
     .references(() => goals.id)
     .notNull(),
