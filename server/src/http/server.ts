22389b26a9c87605680ebc1a20f2df1cab43afdc
Barbara Schvuchov <schvuchov.k@gmail.com>
2024-09-12 13:15:39 -0300

    fastify type provider zod para schema do body nas rotas

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,23 +1,28 @@
 import fastify from "fastify";
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
 import { createGoal } from "../functions/create-goal";
 import z from 'zod'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler);    //add config fastify type provider zod
+app.setSerializerCompiler(serializerCompiler);  //add config fastify type provider zod
 
 //criando rota para add goals
-app.post('/goals', async (request) => {
-
-    //zod para criar a validação 
-    const createGoalSchema = z.object({
-        title: z.string(),
-        desiredWeeklyFrequency: z.number().int().min(1).max(7),
-    })
-
-    const body = createGoalSchema.parse(request.body) //parse : caso não tenha as inf ele vai dar erro
+app.post('/goals', {
+    schema: {
+        body: z.object({
+            title: z.string(),
+            desiredWeeklyFrequency: z.number().int().min(1).max(7),
+        }),
+    }
+}, 
+async (request) => {
+    const { title, desiredWeeklyFrequency } = request.body
 
     await createGoal({
-        title: body.title,
-        desiredWeeklyFrequency: body.desiredWeeklyFrequency,
+        title,
+        desiredWeeklyFrequency,
     })
 })
 
