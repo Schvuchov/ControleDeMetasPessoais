bfb555d3168a917a27b2e2099b9469d2c0a78838
Barbara Schvuchov <schvuchov.k@gmail.com>
2024-09-12 15:59:29 -0300

    funcionalidade queries para get-week-pending-goals

---
+++ server/src/functions/get-week-pending-goals.ts
@@ -0,0 +1,69 @@
+import dayjs from 'dayjs'
+import weekOfYear from 'dayjs/plugin/weekOfYear'
+import { db } from '../db'
+import { goalCompletions, goals } from '../db/schema'
+import { sql, and, lte, gte, count, eq } from 'drizzle-orm'
+
+
+dayjs.extend(weekOfYear)
+
+export async function getWeekPendingGoals() {
+  const firstDayOfWeek = dayjs().startOf('week').toDate()
+  const lastDayOfWeek = dayjs().endOf('week').toDate()
+  
+  //querie pegando todas as metas onde a data de criação é menor ou igual ao ultimo dia da semana
+  const goalsCreatedUpToWeek = db.$with('goals_created_up_to_week').as(
+    db
+      .select({
+        id: goals.id,
+        title: goals.title,
+        desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+        createdAt: goals.createdAt,
+      })
+      .from(goals)
+      .where(lte(goals.createdAt, lastDayOfWeek)) //lte lower than equal
+  )
+
+  //querie para contagem de metas concluidas dentro dessa semana
+  const goalCompletionCounts = db.$with('goal_completion_counts').as(
+    db
+      .select({
+        goalId: goalCompletions.goalId,
+        completionCount: count(goalCompletions.id).as('completionCount'), 
+        //conta quantas vezes aquela meta foi concluida. Precisa de um alias, .as(um nome)
+      })
+      .from(goalCompletions)
+      .where(
+        and(
+          //registros que foram criados
+          gte(goalCompletions.createdAt, firstDayOfWeek), //depois do OU no primeiro dia da semana E
+          lte(goalCompletions.createdAt, lastDayOfWeek) //antes do OU no ultimo dia da semana
+        )
+      )
+      .groupBy(goalCompletions.goalId) //agrupa pelo ID da meta
+  )
+
+  //queria que vai usar as outras queries
+  const pendingGoals = await db
+    .with(goalsCreatedUpToWeek, goalCompletionCounts)
+    .select({
+        id: goalsCreatedUpToWeek.id,
+        title: goalsCreatedUpToWeek.title,
+        desiredWeeklyFrequency: goalsCreatedUpToWeek.desiredWeeklyFrequency,
+        completionCount: sql`
+            COALESCE(${goalCompletionCounts.completionCount}, 0) 
+        `.mapWith(Number),
+
+        //COALESCE  permite fazer um IF caso aquela variavel seja null vai retornar um valor default, no caso 0
+        //mapWith(number) pq o colaesce retorna string
+    })
+    .from(goalsCreatedUpToWeek)
+    .leftJoin(goalCompletionCounts, eq(goalCompletionCounts.goalId, goalsCreatedUpToWeek.id))
+
+    //join onde o goalCompletionCounts.goalId =  goalsCreatedUpToWeek.id
+    //leftJoin pq quero mostrar mesmo que não se tenha completado a meta nenhuma vez 
+
+  return {
+    pendingGoals,
+  }
+}
--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,33 +1,50 @@
-import fastify from "fastify";
-import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
-import { createGoal } from "../functions/create-goal";
+import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createGoal } from '../functions/create-goal'
 import z from 'zod'
+import { getWeekPendingGoals } from '../functions/get-week-pending-goals'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-app.setValidatorCompiler(validatorCompiler);    //add config fastify type provider zod
-app.setSerializerCompiler(serializerCompiler);  //add config fastify type provider zod
+app.setValidatorCompiler(validatorCompiler) //add config fastify type provider zod
+app.setSerializerCompiler(serializerCompiler) //add config fastify type provider zod
+
+//criando rota para ver as pending goals
+app.get('/pending-goals', async () => {
+  const { pendingGoals } = await getWeekPendingGoals()
+
+  return { pendingGoals }
+})
 
 //criando rota para add goals
-app.post('/goals', {
+app.post(
+  '/goals',
+  {
     schema: {
-        body: z.object({
-            title: z.string(),
-            desiredWeeklyFrequency: z.number().int().min(1).max(7),
-        }),
-    }
-}, 
-async (request) => {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async request => {
     const { title, desiredWeeklyFrequency } = request.body
 
     await createGoal({
-        title,
-        desiredWeeklyFrequency,
+      title,
+      desiredWeeklyFrequency,
     })
-})
+  }
+)
 
-app.listen({
+app
+  .listen({
     port: 3333,
-}).then(() => {
+  })
+  .then(() => {
     console.log('HTTP server running!')
-})
+  })
